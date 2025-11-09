2a11d31058dd69dfe6c6e0f102eef57898a7b7dd
Shlavin <nyamburawahu16@gmail.com>
2025-11-09 13:43:28 +0300

    Django Views and URL Configuration

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import DetailView
-from .models import Book, Library
+from .models import Book
+from .models import Library
 
 
 # Function-Based View — List all books
