6368306ef52acd805aec62f5990479cd5630d2b4
Shlavin <nyamburawahu16@gmail.com>
2025-11-09 14:55:00 +0300

    Implement Role-Based Access Control in Django

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
 
    
-    path('admin-dashboard/', views.admin_view, name='admin'),
+    path('admin-dashboard/', views.admin_view, name='admin_view'),
     path('librarian-dashboard/', views.librarian_view, name='librarian_view'),
     path('member-dashboard/', views.member_view, name='member_view'),
 ]
