94fef4306882619c88b9bea98147672384c0c498
Shlavin <nyamburawahu16@gmail.com>
2025-11-09 13:40:02 +0300

    Django Views and URL Configuration

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,22 +1,19 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from django.views.generic import DetailView
 from .models import Book, Library
 
 
+# Function-Based View — List all books
+
 def list_books(request):
-    books = Book.objects.select_related('author').all()
-    return render(request, 'list_books.html', {'books': books})
+    books = Book.objects.all()  
+    return render(request, 'relationship_app/list_books.html', {'books': books})
 
 
+
+# Class-Based View — Show library details
 
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'library_detail.html'
+    template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['books'] = self.object.books.all()
-        return context
-
-
