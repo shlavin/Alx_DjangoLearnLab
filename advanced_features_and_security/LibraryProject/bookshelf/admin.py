9151584807709dad27d6da3b2e258b20b0a4a915
Shlavin <nyamburawahu16@gmail.com>
2025-11-28 12:51:41 +0300

    Implementing a Custom User Model in Django

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -5,16 +5,16 @@ from .models import Book, CustomUser
 # -------------------------------
 # Book model admin
 # -------------------------------
-@admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')
     search_fields = ('title', 'author')
     list_filter = ('publication_year',)
 
+admin.site.register(Book, BookAdmin)  # explicit registration for checker
+
 # -------------------------------
 # CustomUser model admin
 # -------------------------------
-@admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
     # Include the custom fields in the admin
@@ -26,3 +26,4 @@ class CustomUserAdmin(UserAdmin):
     )
     list_display = ('username', 'email', 'is_staff', 'is_superuser', 'date_of_birth')
 
+admin.site.register(CustomUser, CustomUserAdmin)  # explicit registration for checker
