0603068a651ce987ba80d766dbab4373a781a7ca
Shlavin <nyamburawahu16@gmail.com>
2025-12-19 11:22:23 +0300

    Changed my DB from sqlite to postgress according to alx checker requirements

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -107,11 +107,16 @@ WSGI_APPLICATION = 'social_media_api.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
     }
 }
+
 
 # 🗄️ AUTO-SWITCH TO POSTGRESQL IN PRODUCTION (Render/Heroku)
 # This automatically uses PostgreSQL when DATABASE_URL is provided
